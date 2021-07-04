b56d0ae5efc58c091adcf8b87ad4a9df8fcf41ae
karate <karate@users.noreply.github.com>
2021-07-04 15:15:13 +0300

    Add options (speed and color)

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,13 @@ class App {
 		this.brushSize = 1;
 		var canvas = document.getElementById('sketch');
 		this.ctx = canvas.getContext("2d");
-		this.ctx.fillStyle = "#444";
+		this.colors = {
+			'red': '#ce0000',
+			'black': '#444'
+		}
+		this.currentColor = 'black';
+
+		this.speed = 1;
 
 		this.pos = {
 			x: 400,
@@ -38,9 +44,16 @@ class App {
 	}
 
 	draw() {
+		this.ctx.fillStyle = this.colors[this.currentColor];
 		this.ctx.beginPath();
 		this.ctx.arc(this.pos.x, this.pos.y, this.brushSize, 0, 2 * Math.PI);
 		this.ctx.fill();
+	}
+
+	setColor(color) {
+		if (color == 'black' || color == 'red') {
+			this.currentColor = color;
+		}
 	}
 }
 
@@ -75,7 +88,7 @@ window.onload = function(){
 		delete keysPressed[key];
 	});
 
-	window.setInterval(function(){
+	function drawInterval() {
 		if (keysPressed['a']) {
 			window.app.moveLeft();
 		}
@@ -88,5 +101,33 @@ window.onload = function(){
 		if (keysPressed['l']) {
 			window.app.moveDown();
 		}
-	}, 20);
+	};
+	
+	var interval = setInterval(drawInterval, 20/window.app.speed);
+
+	document.getElementById('speed-half').addEventListener('click', function() {
+		clearInterval(interval);
+		window.app.speed = 0.5;
+		interval = setInterval(drawInterval, 20/window.app.speed);
+	});
+
+	document.getElementById('speed-normal').addEventListener('click', function() {
+		clearInterval(interval);
+		window.app.speed = 1;
+		interval = setInterval(drawInterval, 20/window.app.speed);
+	});
+
+	document.getElementById('speed-double').addEventListener('click', function() {
+		clearInterval(interval);
+		window.app.speed = 2;
+		interval = setInterval(drawInterval, 20/window.app.speed);
+	});
+
+	document.getElementById('color-red').addEventListener('click', function() {
+		window.app.setColor('red');
+	});
+
+	document.getElementById('color-black').addEventListener('click', function() {
+		window.app.setColor('black');
+	});
 }
